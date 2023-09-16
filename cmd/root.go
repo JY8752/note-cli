eda1319d81c43e6b8ec2683ecbbf502f5bcde1a1
yksk171118@gmail.com <yksk171118@gmail.com>
2023-09-16 22:35:46 +0900

    feat: Changed to call Help() and display help when the root command is executed

--- cmd/root.go
+++ cmd/root.go
@@ -10,6 +10,9 @@ var rootCmd = &cobra.Command{
 	Use:   "note-cli",
 	Short: "note-cli is a CLI command tool for creating, writing, and managing note articles",
 	Long:  `note-cli is a CLI command tool for creating, writing, and managing note articles`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return cmd.Help()
+	},
 }
 
 func Execute() {
